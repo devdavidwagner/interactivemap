f2446aba011fd52754407dd0305a3d5864961c8d
David Wagner <david.karl.wagner@live.com>
2023-08-09 19:06:25 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON} from 'react-leaflet';
+import japanGeoJSON from './assets/geoJSON/japan.json';
+const App = () => {
+  const [geoJSONData, setGeoJSONData] = useState(null);
+  const [coordinates, setCoordinates] = useState([51.505, -0.09]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let geoStyle;
+  const handleButtonClick = (latitude, longitude, country) => {
+    console.log('Updating coordinates:', latitude, longitude);
+    setCoordinates([ latitude, longitude ]);
+    setStyle(country);
+  };
+
+  const setStyle = (country) => {
+    if(country == "JAP"){
+      setGeoJSONData(japanGeoJSON);
+      geoStyle = "blue";
+      console.log(geoJSONData);
+    }
+  }
+
+  const renderMenu = () => {
+    return (
+      <div>
+        <h1>Largest Cities</h1>
+        <button type="button" onClick={() => handleButtonClick(35.6762, 139.6503, "JAP")}>
+          Tokyo, Japan
+        </button> 
+        <button type="button" onClick={() => handleButtonClick(6.2088, 106.8456, "IDN")}>
+          Jakarta, Indonesia
+        </button> 
+        <button type="button" onClick={() => handleButtonClick(31.2304, 121.4737, "CHN")}>
+          Shanghai, China
+        </button> 
+        <button type="button" onClick={() => handleButtonClick(40.7128, -74.0060, "USA")}>
+          New York City, USA
+        </button> 
+        <button type="button" onClick={() => handleButtonClick(37.5519, 126.9918, "SKO")}>
+          Seoul, South Korea
+        </button> 
+      </div>
+    );
+  };
+
+  const renderMap = () => { 
+    return (
+      <MapContainer center={coordinates} zoom={3} style={{height: '100vh'}}>
+        <TileLayer
+         noWrap={true}
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/>
+        <Marker position={coordinates}>
+          <Popup>
+            A marker indicating a location near London.
+          </Popup>
+        </Marker>
+        {geoJSONData && <GeoJSON data={geoJSONData} style={geoStyle} />}
+      </MapContainer>
+    );
+  };
+
+  return(
+    <div>
+      {renderMenu()}
+      {renderMap()}
     </div>
   );
-}
+
+};
 
 export default App;
